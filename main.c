8a4b77edc11520d63d0cadbd0cff303d900713d9
nyxen <sammizafar203@gmail.com>
2026-09-25 19:58:36 +0530

    Refactor rock-paper-scissors game logic
    Refactor rock-paper-scissors game logic to improve readability and functionality. Added input handling for quitting the game and updated scoring display.

--- main.c
+++ main.c
@@ -5,85 +5,91 @@
 int main()
 {
     srand(time(0));
-    int player, computer = rand() % 3;
-    int computerscore, playerscore;
-    char* choises[]={"rock", "paper", "scissors"};
+    char player[10];
+    int computerscore = 0;
+    int playerscore = 0;
+    char *choices[] = {"rock", "paper", "scissors"};
     /*rock->0
       paper->1
       scissors->2
     */
-   for (int i = 1; i < 6; i++)
-   {
-    int player, computer = rand() % 3;
-    printf("Choose 0 for rock\nChoose 1 for paper\nChoose 2 for scissors\n");
-    scanf("%d", &player);
-    printf("Computer chooses %s\n", choises[computer]);
+    for (int i = 1; i <= 5; i++)
+    {
+        int computer = rand() % 3;
 
-    if (player == 0 && computer == 0)
-    {
-        printf("withdraw\n");
+        printf("Choose 0 for rock🪨\nChoose 1 for paper📄\nChoose 2 for scissors✂️\n");
+        printf("Enter q to quit\n");
+        scanf("%s", player);
+
+        printf("Computer chooses %s\n", choices[computer]);
+        if (player[0] == 'q' || player[0] == 'Q')
+        {
+            break;
+        }
+        if (player[0] == '0' && computer == 0)
+        {
+            printf("Draw\n");
+        }
+        else if (player[0] == '0' && computer == 1)
+        {
+            printf("computer wins\n");
+            computerscore++;
+        }
+        else if (player[0] == '0' && computer == 2)
+        {
+            printf("<*|Player wins|*>\n");
+            playerscore++;
+        }
+        else if (player[0] == '1' && computer == 0)
+        {
+            printf("<*|Player wins|*>\n");
+            playerscore++;
+        }
+        else if (player[0] == '1' && computer == 1)
+        {
+            printf("Draw\n");
+        }
+        else if (player[0] == '1' && computer == 2)
+        {
+            printf("computer wins\n");
+            computerscore++;
+        }
+        else if (player[0] == '2' && computer == 0)
+        {
+            printf("computer wins\n");
+            computerscore++;
+        }
+        else if (player[0] == '2' && computer == 1)
+        {
+            printf("<*|player wins*|>\n");
+            playerscore++;
+        }
+        else if (player[0] == '2' && computer == 2)
+        {
+            printf("Draw\n");
+        }
+        else
+        {
+            printf("invalid input\n");
+        }
     }
-    else if (player == 0 && computer == 1)
+    printf("----*resuls*----\n");
+    printf("player  |-| computer \n");
+    printf("   %d    |-|    %d\n", playerscore, computerscore);
+    printf("-----------------\n");
+
+    if (playerscore > computerscore)
     {
-        printf("computer wins\n");
-        computerscore++;
+        printf("Congratulations! YOU WIN🏆\n");
     }
-    else if (player == 0 && computer == 2)
+    else if (computerscore > playerscore)
     {
-        printf("Player wins\n");
-        playerscore++;
-    }
-    else if (player == 1 && computer == 0)
-    {
-        printf("Player wins\n");
-        playerscore++;
-    }
-    else if (player == 1 && computer == 1)
-    {
-        printf("withdraw\n");
-    }
-    else if (player == 1 && computer == 2)
-    {
-        printf("computer wins\n");
-        computerscore++;
-    }
-    else if (player == 2 && computer == 0)
-    {
-        printf("computer wins\n");
-        computerscore++;
-    }
-    else if (player == 2 && computer == 1)
-    {
-        printf("Player wins\n");
-        playerscore++;
-    }
-    else if (player == 2 && computer == 2)
-    {
-        printf("withdraw\n");
+        printf("Computer Wins\n");
     }
     else
     {
-        printf("invalid input\n");
+        printf("Match draws\n");
     }
-   }
-   printf("--*resuls*--\n");
-   printf("player %d - computer %d\n",playerscore, computerscore);
-
-   if (playerscore>computerscore)
-   {
-    printf("you wins\n");
-   }
-   else if (computerscore>playerscore)
-   {
-    printf("Computer wins\n");
-   }
-   else
-   {
-    printf("Match draws\n");
-   }
-   
-   
-
 
     return 0;
 }
